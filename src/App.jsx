327c9217b9895200ae5052633b4ece221d9d4e87
Jonatan P <jonatan2502@gmail.com>
2022-12-06 22:31:55 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Cards from './components/Cards/Cards'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
       <h1>Practica M2</h1>
-      <Cards></Cards>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Cards/>}/>
+          {/* <Route path='' component={}/> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
